4f9eda7da7acf2cd64fa90566c79190fec8ce2d5
Peter Westmacott <peter.westmacott@gmail.com>
2019-01-25 22:27:18 +0000

    Initial commit.

---
+++ src/fabric_patterns/core.clj
@@ -0,0 +1,141 @@
+(ns fabric-patterns.core
+  (:import
+    [java.awt.image BufferedImage]
+    [javax.swing JFrame WindowConstants]
+    [java.awt Graphics Dimension Graphics2D RenderingHints Color BasicStroke]))
+
+
+(defn create-image [name width height]
+  (let [image (BufferedImage. width height BufferedImage/TYPE_INT_RGB)
+        graphics (.getGraphics image)
+        frame (proxy [JFrame] []
+                (paint [^Graphics graphics]
+                  (let [y (- (.getHeight (.getSize this))
+                             (.getHeight (.getContentPane this)))]
+                    (.drawImage graphics image 0 y this))))]
+
+    (.setRenderingHint ^Graphics2D graphics RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
+
+    (doto ^JFrame frame
+      (.setTitle name)
+      (-> .getContentPane (.setPreferredSize (Dimension. (.getWidth image nil) (.getHeight image nil))))
+      (.setDefaultCloseOperation WindowConstants/HIDE_ON_CLOSE)
+      .pack
+      (.setVisible true))
+
+    {:image image
+     :width (.getWidth image nil)
+     :height (.getHeight image nil)
+     :graphics graphics
+     :frame frame
+     :repaint #(.repaint frame)
+     ;:contains? (fn [x y]
+     ;             (and (< 0 x width)
+     ;                  (< 0 y height)))
+     }))
+
+(defn new-colour
+  ([r g b]
+   (Color. r g b))
+  ([r g b a]
+   (Color. r g b a)))
+
+(defn clear [{:keys [graphics width height repaint]} colour]
+  (.setColor graphics colour)
+  (.fillRect graphics 0 0 width height)
+  (repaint))
+
+(defn circle [{:keys [graphics repaint]} x y r colour width]
+  (.setStroke graphics (BasicStroke. width))
+  (.setColor graphics colour)
+  (.drawOval graphics (- x r) (- y r) (* 2 r) (* 2 r))
+  (repaint))
+
+(defn arc [{:keys [graphics repaint]} x y r start length colour width]
+  (.setStroke graphics (BasicStroke. width BasicStroke/CAP_ROUND BasicStroke/JOIN_ROUND))
+  (.setColor graphics colour)
+  ;(.drawOval graphics (- x r) (- y r) (* 2 r) (* 2 r))
+  (.drawArc graphics (- x r) (- y r) (* 2 r) (* 2 r) start length)
+  (repaint))
+
+(def sample (create-image "Sample" 1000 1000))
+
+(def green (new-colour 100 255 70))
+(def pink (new-colour 255 25 255))
+(def dark-pink (new-colour 150 0 50))
+(def highlight (new-colour 255 255 255 127))
+(def lowlight (new-colour 0 0 0 127))
+
+(do
+
+  (clear sample dark-pink)
+
+
+  ;(circle sample 100 100 100 (new-colour 100 255 70) 5)
+
+  (let [ring-count 49]
+    (doseq [[y colour r] (map vector
+                              (range 0 1000 (/ 1000 ring-count))
+                              (cycle [pink green])
+                              (map #(+ 125 (* 50 (Math/cos %)))
+                                   (range 0 (* 2 Math/PI) (/ (* 2 Math/PI) ring-count)))
+                              )
+            [x-offset y-offset start-arc] (map vector
+                                               (range 0 1000 250)
+                                               (cycle [0 500])
+                                               (cycle [0 180]))]
+
+      (doseq [wrap-x [-1000 0 1000]
+              wrap-y [-1000 0 1000]]
+        ;(circle sample (+ wrap-x x-offset) (+ wrap-y y y-offset) r colour 3)
+        (arc sample (+ wrap-x x-offset) (+ wrap-y y y-offset) r start-arc 180 colour 3)
+        (arc sample (+ wrap-x x-offset) (+ wrap-y y y-offset) r (+ start-arc 45) 20 highlight 5)
+        (arc sample (+ wrap-x x-offset) (+ wrap-y y y-offset) r (+ start-arc 135) 45 lowlight 3)
+
+
+        )
+
+      ))
+
+  )
+
+
+;(defn rgb->int [r g b]
+;  (bit-or (bit-shift-left r 16)
+;          (bit-shift-left g 8)
+;          b))
+
+;(defn draw
+;  ([image x y r g b]
+;   (draw image x y (rgb->int r g b)))
+;  ([image x y rgb]
+;   (when ((:contains? image) x y)
+;     (.setRGB (:image image) x y rgb)
+;     (.repaint (:frame image)))))
+
+
+;(doseq [x (range 400) y (range 1000)]
+;  (draw b x y 255 255 255))
+
+
+; what are you doing?
+; use the graphics object like you always used to
+; use anti-aliasing
+
+;(let [green (rgb->int 0 255 0)
+;      pink (rgb->int 255 0 255)
+;      circle-data {:radius (map #(+ 100 (* 75 (Math/sin %))) (range 0 100 0.2))
+;                   :x-offset (repeat 120)
+;                   :y-offset (range -100 1400 20)
+;                   :colour (cycle [pink green])}
+;      circles (take 60 (apply map (fn [& args] (zipmap (keys circle-data) args)) (vals circle-data)))]
+;  (doseq [theta (range (/ (- Math/PI) 2) (/ Math/PI 2) 0.001)]
+;    (let [x (Math/sin theta)
+;          y (Math/cos theta)]
+;      (doseq [{:keys [radius x-offset y-offset colour]} circles]
+;        (doseq [r (range radius (+ radius 5))]
+;          (doseq [[xa ya] [[0 0] [400 400] [800 0]]]
+;            (draw b
+;                  (+ xa x-offset (* x r))
+;                  (+ ya y-offset (* y r))
+;                  colour)))))))
