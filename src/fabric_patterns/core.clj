9b218b6a12f58528e26ecc8edfa28f0a7f1d4aa3
Peter Westmacott <peter.westmacott@gmail.com>
2019-01-27 11:12:47 +0000

    Progress.

--- src/fabric_patterns/core.clj
+++ src/fabric_patterns/core.clj
@@ -1,9 +1,13 @@
 (ns fabric-patterns.core
+  (:require
+    [clojure.java.io :as io])
   (:import
-    [java.awt.image BufferedImage]
+    [java.awt.image BufferedImage RenderedImage]
     [javax.swing JFrame WindowConstants]
-    [java.awt Graphics Dimension Graphics2D RenderingHints Color BasicStroke]
-    [java.awt.geom Ellipse2D$Float]))
+    [java.awt Graphics Dimension Graphics2D RenderingHints Color BasicStroke Polygon]
+    [java.awt.geom Ellipse2D$Float]
+    [javax.imageio ImageIO]
+    [java.io File]))
 
 
 (defn create-image [name width height]
@@ -24,12 +28,12 @@
       .pack
       (.setVisible true))
 
-    {:image image
-     :width (.getWidth image nil)
-     :height (.getHeight image nil)
+    {:image    image
+     :width    (.getWidth image nil)
+     :height   (.getHeight image nil)
      :graphics graphics
-     :frame frame
-     :repaint #(.repaint frame)
+     :frame    frame
+     :repaint  #(.repaint frame)
      ;:contains? (fn [x y]
      ;             (and (< 0 x width)
      ;                  (< 0 y height)))
@@ -41,24 +45,134 @@
   ([r g b a]
    (Color. r g b a)))
 
-(defn clear [{:keys [graphics width height repaint]} colour]
-  (.setColor graphics colour)
-  (.fillRect graphics 0 0 width height)
-  (repaint))
+(defn clear [colour]
+  {:op     :clear
+   :colour colour
+   :z      Integer/MIN_VALUE})
 
-(defn circle [{:keys [graphics repaint]} x y r colour width]
-  (.setStroke graphics (BasicStroke. width))
-  (.setColor graphics colour)
-  (.drawOval graphics (- x r) (- y r) (* 2 r) (* 2 r))
-  (repaint))
+(defn circle [x y z r style]
+  {:op     :circle
+   :x      x
+   :y      y
+   :z      z
+   :r      r
+   :style style})
 
-(defn arc [{:keys [graphics repaint]} x y r start length colour width]
-  (.setStroke graphics (BasicStroke. width BasicStroke/CAP_ROUND BasicStroke/JOIN_ROUND))
-  (.setColor graphics colour)
-  ;(.drawOval graphics (- x r) (- y r) (* 2 r) (* 2 r))
-  (.drawArc graphics (- x r) (- y r) (* 2 r) (* 2 r) start length)
-  (repaint))
+(defn line [x1 y1 x2 y2 z style]
+  {:op     :line
+   :x1     x1
+   :y1     y1
+   :x2     x2
+   :y2     y2
+   :z      z
+   :style style})
 
+(defn arc [x y z r start length style]
+  {:op     :arc
+   :x      x
+   :y      y
+   :z      z
+   :r      r
+   :start  start
+   :length length
+   :style style})
+
+(defn blob [x y z r colour]
+  {:op     :blob
+   :x      x
+   :y      y
+   :z      z
+   :r      r
+   :colour colour})
+
+(defn polygon [x-coords y-coords z style]
+  {:op      :polygon
+   ;:polygon (Polygon. x-coords y-coords (count x-coords))
+   :xs x-coords
+   :ys y-coords
+   :z       z
+   :style   style})
+
+(defn wrap [instruction]
+  (cond
+    (and (:x instruction)
+         (:y instruction)) (for [wrap-x [-1000 0 1000]
+                                     wrap-y [-1000 0 1000]]
+                                 (-> instruction
+                                     (update :x + wrap-x)
+                                     (update :y + wrap-y)
+                                     (update :z + wrap-x wrap-y)))
+    :else instruction))
+
+;(defn blob [{:keys [graphics repaint] :as image} x y radius colour]
+;  (with-clip graphics (Ellipse2D$Float. (- x radius) (- y radius) (* 2 radius) (* 2 radius))
+;    (let [highlight-offset 0.3]
+;      (doseq [i (range 0 1 0.01)]
+;        (let [ring-radius (* i radius (+ 1.2 highlight-offset))
+;              light-factor (- 1.5 i)
+;              light-bump (* 50 (- 1.5 i))
+;              re-light (fn [v] (int (min 255 (* (+ v light-bump) light-factor))))]
+;          (circle image
+;                  (+ x (* radius highlight-offset))
+;                  (- y (* radius highlight-offset))
+;                  ring-radius
+;                  (Color. (re-light (.getRed colour))
+;                          (re-light (.getGreen colour))
+;                          (re-light (.getBlue colour)))
+;                  2)))))
+;  (repaint))
+
+(def sample (create-image "Sample" 1000 1000))
+
+(def green (new-colour 100 255 70))
+(def pink (new-colour 255 25 255))
+(def dark-pink (new-colour 150 0 50))
+(def highlight (new-colour 255 255 255 127))
+(def lowlight (new-colour 0 0 0 127))
+(def black Color/BLACK)
+
+(defn re-light [colour bump factor]
+  (let [adjust (fn [v]
+                 (int (max 0 (min 255 (* (+ v bump) factor)))))]
+    (Color. ^Integer (adjust (.getRed colour))
+            ^Integer (adjust (.getGreen colour))
+            ^Integer (adjust (.getBlue colour)))))
+
+(defn lighter [colour]
+  (re-light colour 10 1.1))
+
+(defn darker [colour]
+  (re-light colour -10 0.9))
+
+(defn lighter-or-darker [colour]
+  (re-light colour (- (rand 20) 10) (+ 0.9 (rand 0.2))))
+
+(defn much [f x]
+  (f (f (f x))))
+
+
+(defn stroke [colour width]
+  {:stroke {:colour colour
+            :width width}})
+
+(defn fill [colour]
+  {:fill {:colour colour}})
+
+(defmacro do-stroke [graphics style stroke-expr]
+  `(when (:stroke ~style)
+     (.setStroke ~graphics (BasicStroke. (:width (:stroke ~style)) BasicStroke/CAP_ROUND BasicStroke/JOIN_ROUND))
+    (.setColor ~graphics (:colour (:stroke ~style)))
+    ~stroke-expr))
+
+(defmacro do-fill [graphics style fill-expr]
+  `(when (:fill ~style)
+     (.setColor ~graphics (:colour (:fill ~style)))
+     ~fill-expr))
+
+(defmacro styled [graphics style stroke-expr fill-expr]
+  `(do
+     (do-stroke ~graphics ~style ~stroke-expr)
+     (do-fill ~graphics ~style ~fill-expr)))
 
 (defmacro with-clip [graphics clip & body]
   `(let [previous-clip# (.getClip ~graphics)]
@@ -68,66 +182,202 @@
        (finally
          (.setClip ~graphics previous-clip#)))))
 
-(defn blob [{:keys [graphics repaint] :as image} x y radius colour]
-  (with-clip graphics (Ellipse2D$Float. (- x radius) (- y radius) (* 2 radius) (* 2 radius))
-    (let [highlight-offset 0.3]
-      (doseq [i (range 0 1 0.01)]
-        (let [ring-radius (* i radius (+ 1.2 highlight-offset))
-              light-factor (- 1.5 i)
-              light-bump (* 50 (- 1.5 i))
-              re-light (fn [v] (int (min 255 (* (+ v light-bump) light-factor))))]
-          (circle image
-                  (+ x (* radius highlight-offset))
-                  (- y (* radius highlight-offset))
-                  ring-radius
-                  (Color. (re-light (.getRed colour))
-                          (re-light (.getGreen colour))
-                          (re-light (.getBlue colour)))
-                  2)))))
+(defmulti wrap (fn [instruction] (:op instruction)))
+
+(defmethod wrap :default [instruction]
+  (for [wrap-x [-1000 0 1000]
+        wrap-y [-1000 0 1000]]
+    (-> instruction
+        (update :x + wrap-x)
+        (update :y + wrap-y)
+        (update :z + wrap-x wrap-y))))
+
+(defmethod wrap :clear [instruction] instruction)
+
+(defmethod wrap :line [instruction]
+  (for [wrap-x [-1000 0 1000]
+        wrap-y [-1000 0 1000]]
+    (-> instruction
+        (update :x1 + wrap-x)
+        (update :y1 + wrap-y)
+        (update :x2 + wrap-x)
+        (update :y2 + wrap-y)
+        (update :z + wrap-x wrap-y))))
+
+(defmethod wrap :polygon [instruction]
+  (for [wrap-x [-1000 0 1000]
+        wrap-y [-1000 0 1000]]
+    (-> instruction
+        (update :xs #(map (partial + wrap-x) %))
+        (update :ys #(map (partial + wrap-y) %))
+        (update :z + wrap-x wrap-y))))
+
+(defmulti render (fn [_image_ instruction] (:op instruction)))
+
+(defmethod render :circle [{:keys [graphics repaint]} {:keys [x y r style]}]
+  (styled graphics style
+          (.drawOval graphics (- x r) (- y r) (* 2 r) (* 2 r))
+          (.fillOval graphics (- x r) (- y r) (* 2 r) (* 2 r)))
   (repaint))
 
-(def sample (create-image "Sample" 1000 1000))
+(defmethod render :arc [{:keys [graphics repaint]} {:keys [x y r start length style]}]
+  (styled graphics style
+          (.drawArc graphics (- x r) (- y r) (* 2 r) (* 2 r) start length)
+          (.fillArc graphics (- x r) (- y r) (* 2 r) (* 2 r) start length))
+  (repaint))
 
-(def green (new-colour 100 255 70))
-(def pink (new-colour 255 25 255))
-(def dark-pink (new-colour 150 0 50))
-(def highlight (new-colour 255 255 255 127))
-(def lowlight (new-colour 0 0 0 127))
+(defmethod render :clear [{:keys [graphics width height repaint]} {:keys [colour]}]
+  (.setColor graphics colour)
+  (.fillRect graphics 0 0 width height)
+  (repaint))
 
+(defmethod render :line [{:keys [graphics repaint]} {:keys [x1 y1 x2 y2 style]}]
+  (styled graphics style
+          (.drawLine graphics x1 y1 x2 y2)
+          (.drawLine graphics x1 y1 x2 y2))
+  (repaint))
 
+(defmethod render :blob [{:keys [graphics repaint]} {:keys [x y r colour]}]
+  (with-clip graphics (Ellipse2D$Float. (- x r) (- y r) (* 2 r) (* 2 r))
+    (let [highlight-offset 0.3]
+      (doseq [i (range 0 1 0.005)]
+        (let [ring-radius (* i r (+ 1.2 highlight-offset))
+              light-factor (- 1.5 i)
+              light-bump (* 50 (- 1.5 i))
+              size (* 2 ring-radius)]
+          (.setStroke graphics (BasicStroke. 2))
+          (.setColor graphics (re-light colour light-bump light-factor))
+          (.drawOval graphics
+                     (- (+ x (* r highlight-offset)) ring-radius)
+                     (- (- y (* r highlight-offset)) ring-radius) size size)))))
+  (repaint))
 
+(defmethod render :polygon [{:keys [graphics repaint]} {:keys [xs ys style]}]
+  (let [polygon (Polygon. (int-array xs) (int-array ys) (count xs))]
+    (styled graphics style
+            (.drawPolygon graphics polygon)
+            (.fillPolygon graphics polygon)))
+  (repaint))
 
 (do
+  (->> (concat
+         [(clear dark-pink)]
 
-  (clear sample dark-pink)
+         (let [ring-count 50]
+           (for [[y r] (map vector
+                            (range 0 1000 (/ 1000 ring-count))
+                            (map #(+ 60 (* 50 (Math/cos %)))
+                                 (range 0 (* 2 Math/PI) (/ (* 2 Math/PI) ring-count))))
+                 [x-offset y-offset] (map vector
+                                          (range 0 1000 250)
+                                          (cycle [0 500]))]
+             (let [colour (much lighter-or-darker pink)]
+               [(wrap (arc x-offset (+ y y-offset) 10 r 0 180 (stroke (->> colour darker darker darker) 1)))
+                (wrap (arc x-offset (+ y y-offset) 10 r 10 150 (stroke (->> colour darker darker) 2)))
+                (wrap (arc x-offset (+ y y-offset) 10 r 20 120 (stroke (->> colour darker) 3)))
+                (wrap (arc x-offset (+ y y-offset) 10 r 30 90 (stroke (->> colour) 4)))
+                (wrap (arc x-offset (+ y y-offset) 10 r 40 60 (stroke (->> colour (much lighter)) 5)))
+                (wrap (arc x-offset (+ y y-offset) 10 r 50 30 (stroke (->> colour (much lighter) (much lighter)) 6)))
+                (wrap (arc x-offset (+ y y-offset) 10 r 60 10 (stroke (->> colour (much lighter) (much lighter) (much lighter)) 7)))
+                ;(wrap (arc x-offset (+ y y-offset) 10 r 0 180 colour 3))
+                ;(wrap (arc x-offset (+ y y-offset) 10 r 45 20 highlight 5))
+                ;(wrap (arc x-offset (+ y y-offset) 10 r 135 45 lowlight 3))
+                ])))
+
+         (for [[y r] (map vector
+                          [-260 -160 0 160 260]
+                          [10 40 75 40 10])
+               [x-offset y-offset] (map vector
+                                        (range 0 1000 250)
+                                        (cycle [0 500]))]
+           [(wrap (blob x-offset (+ y y-offset) -2001 r green))
+            (wrap (circle x-offset (+ y y-offset) -2000 r (stroke black 1)))])
+
+         (for [i (range 0 1 (/ 1 16))
+               [x-offset y-offset] (map vector
+                                        (range 0 1000 250)
+                                        (cycle [0 500]))]
+
+           (let [y (+ (* i 1000) y-offset)
+                 theta (* i 2 Math/PI)
+                 x (+ (+ 120 (* 50 (Math/cos theta))) x-offset)
+                 leaf-colour (much lighter-or-darker (much lighter-or-darker (much lighter-or-darker (much lighter-or-darker green))))
+                 leaf-length 25]
 
 
-  ;(circle sample 100 100 100 (new-colour 100 255 70) 5)
 
-  (let [ring-count 50]
-    (doseq [[y colour r] (map vector
-                              (range 0 1000 (/ 1000 ring-count))
-                              (cycle [pink green])
-                              (map #(+ 60 (* 50 (Math/cos %)))
-                                   (range 0 (* 2 Math/PI) (/ (* 2 Math/PI) ring-count)))
-                              )
-            [x-offset y-offset] (map vector
-                                     (range 0 1000 250)
-                                     (cycle [0 500]))]
+             ; each leaf
+             (let [leaf-points (for [n
+                                     ;(range (- leaf-length) leaf-length 4)
+                                     (range (- leaf-length) (inc leaf-length) 2)
+                                     ;[-20 -15 -10 -5 0 5 10 15 20]
+                                     ]
 
-      (doseq [wrap-x [-1000 0 1000]
-              wrap-y [-1000 0 1000]]
-        ;(circle sample (+ wrap-x x-offset) (+ wrap-y y y-offset) r colour 3)
-        (arc sample (+ wrap-x x-offset) (+ wrap-y y y-offset) r 0 180 colour 3)
-        (arc sample (+ wrap-x x-offset) (+ wrap-y y y-offset) r 45 20 highlight 5)
-        (arc sample (+ wrap-x x-offset) (+ wrap-y y y-offset) r 135 45 lowlight 3)
+                                 (let [xa (* n (Math/sin theta))
+                                       ya (* n (Math/cos theta))
+                                       leaf-width (+ 6 (* 6 (Math/cos (* (/ n leaf-length) Math/PI))))
+                                       xb (* leaf-width (Math/cos theta))
+                                       yb (- (* leaf-width (Math/sin theta)))
 
+                                       x1 (+ x xa xb)
+                                       y1 (+ y ya yb)
+                                       x2 (- (+ x xa) xb)
+                                       y2 (- (+ y ya) yb)]
 
-        )
+                                   [[x1 y1] [x2 y2]]
 
-      ))
+                                   ;[(wrap (line x1 y1 x2 y2 0 leaf-colour 1))]
 
-  )
+                                   ))
+                   half-one (map first leaf-points)
+                   half-two (reverse (map second leaf-points))
+                   x-coords (concat (map first half-one) (map first half-two))
+                   y-coords (concat (map second half-one) (map second half-two))]
+
+               (wrap (polygon x-coords y-coords 0 (merge (fill leaf-colour) (stroke black 1))))
+
+               )))
+
+         )
+       flatten
+       (sort-by :z)
+       (mapv #(render sample %))
+       first)
+
+  ; ImageIO.write(image, "png", new File("./" + filename + ".png"));
+  (ImageIO/write ^RenderedImage (:image sample) "png" (io/file (str "sample.png"))))
+
+;(do
+;
+;  (clear sample dark-pink)
+;
+;
+;  ;(circle sample 100 100 100 (new-colour 100 255 70) 5)
+;
+;  (let [ring-count 50]
+;    (doseq [[y colour r] (map vector
+;                              (range 0 1000 (/ 1000 ring-count))
+;                              (cycle [pink green])
+;                              (map #(+ 60 (* 50 (Math/cos %)))
+;                                   (range 0 (* 2 Math/PI) (/ (* 2 Math/PI) ring-count)))
+;                              )
+;            [x-offset y-offset] (map vector
+;                                     (range 0 1000 250)
+;                                     (cycle [0 500]))]
+;
+;      (doseq [wrap-x [-1000 0 1000]
+;              wrap-y [-1000 0 1000]]
+;        ;(circle sample (+ wrap-x x-offset) (+ wrap-y y y-offset) r colour 3)
+;        (arc sample (+ wrap-x x-offset) (+ wrap-y y y-offset) r 0 180 colour 3)
+;        (arc sample (+ wrap-x x-offset) (+ wrap-y y y-offset) r 45 20 highlight 5)
+;        (arc sample (+ wrap-x x-offset) (+ wrap-y y y-offset) r 135 45 lowlight 3)
+;
+;
+;        )
+;
+;      ))
+;
+;  )
 
 ;(defn rgb->int [r g b]
 ;  (bit-or (bit-shift-left r 16)
